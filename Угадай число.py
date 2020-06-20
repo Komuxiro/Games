6383a96d7a1dcde2c003adedb46846537d309b24
denisalis666@gmail.com <ujkjdfxtd382>
2020-06-20 16:05:26 +0300

    Угадай число / number guessing game

---
+++ Угадай число.py
@@ -0,0 +1,26 @@
+# Игра для угадывания чисел / Number guessing game
+import random
+
+guessesTaken = 0  # переменная которая будет считать колличество поппыток
+
+print('Привет! Как тебя зовут?')
+myName = input()
+number = random.randint(1, 20)  # вызываем функцию randint, указывая диапазон згадываемых чисел
+print('Что ж, ' + myName, 'я загадл число от 1 до 20.')
+
+for guessesTaken in range(6):  # указываем колличество циклов которое дается игроку на угадывание числа
+    print('Попробуй угадать.')
+    guess = int(input())
+
+    if guess < number:
+        print('Твое число слишко маленькое')
+    elif guess > number:
+        print('Твое число слишком большое')
+    elif guess == number:
+        guessesTaken = str(guessesTaken + 1)
+        print('Отлично, ' + myName + '! Ты справился за ' + guessesTaken + ' попытки!')
+        break
+
+if guess != number:
+    number = str(number)
+    print('Увы ты не справился. Я загадал число' + number + '.')
